8b176345f506f1c5a45ef516240441304ebc83f7
Chinmay Sawant <csawant@algoanalytics.com>
2020-09-26 12:38:52 +0530

    Refactored code

--- opencv_based/cartoonizer.py
+++ opencv_based/cartoonizer.py
@@ -4,46 +4,40 @@ import time
 from scipy import ndimage
 from scipy.ndimage.filters import convolve
 
-def sobel_filters( img):
-    Kx = np.array([[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]], np.float32)
-    Ky = np.array([[1, 2, 1], [0, 0, 0], [-1, -2, -1]], np.float32)
-
-    Ix = ndimage.filters.convolve(img, Kx)
-    Iy = ndimage.filters.convolve(img, Ky)
-
-    G = np.hypot(Ix, Iy)
-    G = G / G.max() * 255
-    theta = np.arctan2(Iy, Ix)
-    return (G, theta)
-
-def gaussian_kernel(size, sigma=1):
-    size = int(size) // 2
-    x, y = np.mgrid[-size:size+1, -size:size+1]
-    normal = 1 / (2.0 * np.pi * sigma**2)
-    g =  np.exp(-((x**2 + y**2) / (2.0*sigma**2))) * normal
-    return g
 
 class Cartoonizer: 
 
     def __init__(self): 
         pass
 
+
+    def gaussian_kernel(self, size, sigma=1):
+        size = int(size) // 2
+        x, y = np.mgrid[-size:size+1, -size:size+1]
+        normal = 1 / (2.0 * np.pi * sigma**2)
+        g =  np.exp(-((x**2 + y**2) / (2.0*sigma**2))) * normal
+        return g
+
+
+    def bilateral_filter(self, image):
+        num_filters = 10
+        for _ in range(num_filters):
+            img_color = cv2.bilateralFilter(image, 7, 9, 9) 
+        return img_color
+    
+
+    def edge_detection(self, image):
+        img_gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+        # img_edge = cv2.Canny(img_gray,100,220)
+        img_smoothed = convolve(img_gray, self.gaussian_kernel(9, 1.4))
+        img_edge = cv2.Canny(img_smoothed,100,200)
+        return img_edge
+
+
     def render(self, img_rgb):   
-        numBilateralFilters = 10 
-
-        img_color = img_rgb 
-       
-        for _ in range(numBilateralFilters): 
-            img_color = cv2.bilateralFilter(img_color, 7, 9, 9) 
-
-        # Canny edge detector
-        img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_RGB2GRAY)
-        # img_edge = cv2.Canny(img_gray,150,220)
-
-
-        img_smoothed = convolve(img_gray, gaussian_kernel(9, 1.4))
-        img_edge = cv2.Canny(img_smoothed,150,220)
-         
-        img_color[img_edge >=255] = [53, 53, 53]
+        img_color = self.bilateral_filter(img_rgb) 
+        img_edge = self.edge_detection(img_rgb)
+        img_color[img_edge == 255] = [53, 53, 53]
+        # img_color = cv2.bitwise_and(img_color, 255-img_edge)
         return img_color
 
--- opencv_based/read_video.py
+++ opencv_based/read_video.py
@@ -1,38 +1,43 @@
-# importing libraries 
+import argparse
 import cv2 
 import numpy as np 
 from cartoonizer import Cartoonizer
 
 
 tmp_canvas = Cartoonizer() 
-cap = cv2.VideoCapture('video_3.mp4') 
 
-frame_width = int(cap.get(3)) 
-frame_height = int(cap.get(4))  
-size = (frame_width, frame_height) 
 
-fourcc  = cv2.VideoWriter_fourcc(*"MJPG") 
-result = cv2.VideoWriter('output.avi',  fourcc, 10, size) 
+def convert_video(cap, result):
+    if (cap.isOpened()== False): 
+        print("Error opening video file") 
 
-# Check if camera opened successfully 
-if (cap.isOpened()== False): 
-    print("Error opening video file") 
+    while(cap.isOpened()): 	 
+        ret, frame = cap.read() 
+        if ret == True: 
+            cartoonze_image = tmp_canvas.render(frame)
+            cartoonze_image = cv2.resize(cartoonze_image, size)
+            cv2.imshow('Frame', cartoonze_image)
+            result.write(cartoonze_image.astype('uint8')) 
 
-# Read until video is completed 
-while(cap.isOpened()): 	 
-    ret, frame = cap.read() 
-    if ret == True: 
-        cartoonze_image = tmp_canvas.render(frame)
-        cartoonze_image = cv2.resize(cartoonze_image, size)
-        cv2.imshow('Frame', cartoonze_image)
-        result.write(cartoonze_image.astype('uint8')) 
-	    
-        if cv2.waitKey(25) & 0xFF == ord('q'): 
-            break
+            if cv2.waitKey(25) & 0xFF == ord('q'): 
+                break
+        else: 
+    	    break
 
-    else: 
-	    break
+    cap.release() 
+    cv2.destroyAllWindows() 
 
- 
-cap.release() 
-cv2.destroyAllWindows() 
+
+
+if __name__ == "__main__":
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-file_name", "--file_name", required=True, help="input video file name")
+    args = vars(ap.parse_args())
+    video_file_name = args['file_name']
+
+    cap = cv2.VideoCapture(video_file_name)   
+    size = (int(cap.get(3)), int(cap.get(4))) 
+
+    fourcc  = cv2.VideoWriter_fourcc(*"MJPG") 
+    result = cv2.VideoWriter('output.avi',  fourcc, 10, size)
+    convert_video(cap, result)
