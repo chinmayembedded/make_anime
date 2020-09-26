455a482cb7589abe34bd71a62c7041a554fe1a59
Chinmay Sawant <csawant@algoanalytics.com>
2020-09-26 21:29:42 +0530

    Added adaptive threshold

--- opencv_based/cartoonizer.py
+++ opencv_based/cartoonizer.py
@@ -29,15 +29,19 @@ class Cartoonizer:
     def edge_detection(self, image):
         img_gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
         # img_edge = cv2.Canny(img_gray,100,220)
-        img_smoothed = convolve(img_gray, self.gaussian_kernel(9, 1.4))
-        img_edge = cv2.Canny(img_smoothed,100,200)
+        # img_smoothed = convolve(img_gray, self.gaussian_kernel(9, 1.4))
+        img_smoothed = cv2.medianBlur(img_gray, 7)
+        img_edge = cv2.adaptiveThreshold(img_smoothed, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, 
+            cv2.THRESH_BINARY, 9, 11)
+        # img_edge = cv2.Canny(img_smoothed,100,200)
         return img_edge
 
 
     def render(self, img_rgb):   
         img_color = self.bilateral_filter(img_rgb) 
         img_edge = self.edge_detection(img_rgb)
-        img_color[img_edge == 255] = [53, 53, 53]
+        # img_color[img_edge == 255] = [53, 53, 53]
+        img_color = cv2.bitwise_and(img_color, img_color, mask=img_edge) 
         # img_color = cv2.bitwise_and(img_color, 255-img_edge)
         return img_color
 
